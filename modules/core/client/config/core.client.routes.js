36b3ecb6b2371b8949e680d2fdf0e7d25f59c6bd
Jonathen Settle <jonathenksettle@ufl.edu>
2016-03-12 13:12:53 -0500

    Working Contact Us link

--- modules/core/client/config/core.client.routes.js
+++ modules/core/client/config/core.client.routes.js
@@ -13,6 +13,10 @@ angular.module('core').config(['$stateProvider', '$urlRouterProvider',
 
     // Home state routing
     $stateProvider
+      .state('contact', {
+        url: '/contact',
+        templateUrl: 'modules/core/client/views/contact.client.view.html'
+      })
     .state('home', {
       url: '/',
       templateUrl: 'modules/core/client/views/home.client.view.html'
