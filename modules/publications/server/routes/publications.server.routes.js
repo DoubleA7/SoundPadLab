9b129fcff23134e36203b02424e7e4aefc8e4298
trident-xL <aq.blackmist@gmail.com>
2016-03-23 01:20:20 -0400

    re-enabled adminPolicy on PUT/POST/DELETE

--- modules/publications/server/routes/publications.server.routes.js
+++ modules/publications/server/routes/publications.server.routes.js
@@ -4,19 +4,19 @@
  * Module dependencies.
  */
 var publications = require('../controllers/publications.server.controller');
-// var adminPolicy = require('../../../users/server/policies/admin.server.policy');
+var adminPolicy = require('../../../users/server/policies/admin.server.policy');
 
 module.exports = function (app) {
     // publications collection routes
   app.route('/api/publications')
         .get(/*adminPolicy.isAllowed,*/publications.list)
-        .post(/*adminPolicy.isAllowed,*/publications.create);
+        .post(adminPolicy.isAllowed,publications.create);
 
     // Single publication routes
   app.route('/api/publications/:publicationId')
         .get(/*adminPolicy.isAllowed,*/publications.read)
-        .put(/*adminPolicy.isAllowed,*/publications.update)
-        .delete(/*adminPolicy.isAllowed,*/publications.delete);
+        .put(adminPolicy.isAllowed,publications.update)
+        .delete(adminPolicy.isAllowed,publications.delete);
 
     // Finish by binding the publication middleware
   app.param('publicationId', publications.publicationByID);
