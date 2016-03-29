1baaf2783c616504612d334ece403290ceff1e92
Ariel <arif93@ufl.edu>
2016-03-29 11:14:40 -0400

    improved the UI for About, Team, and Contact. Also added a few references

--- modules/core/client/controllers/core.client.controller.js
+++ modules/core/client/controllers/core.client.controller.js
@@ -8,5 +8,26 @@ angular.module('core').controller('CoreController', ['$scope', '$state',
     console.log($scope);
     $scope.init = function() {
     };
+    $scope.goToContact = function() {
+      $state.go('core.contact');
+    };
+    $scope.goToTeam = function() {
+      $state.go('core.team');
+    };
+    $scope.goToAbout = function(){
+      $state.go('core.about');
+    };
+    $scope.goToAudio = function(){
+      $state.go('core.audioFiles');
+    };
+    $scope.goToResearch = function(){
+      $state.go('core.researchs');
+    };
+    $scope.goToPublications = function(){
+      $state.go('core.publications');
+    };
+    $scope.goToEvents = function(){
+      $state.go('core.events');
+    };
   }
 ]);
