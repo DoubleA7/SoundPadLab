4386e72fc4dced17326d7fbc0fbb477cd83b46d4
Jonathen Settle <jonathenksettle@ufl.edu>
2016-04-09 18:29:31 -0400

    reCAPTCHA on e-mail form on contact page

--- modules/core/client/controllers/contact.client.controller.js
+++ modules/core/client/controllers/contact.client.controller.js
@@ -7,11 +7,13 @@ angular.module('core').controller('ContactController', ['$scope', '$http',
     $scope.email = '';
     $scope.verifyEmail = 'test2';
     $scope.msg = '';
+    $scope.captcha = '';
 
     var data = ({
       name : $scope.name,
       email : $scope.email,
-      msg : $scope.msg
+      msg : $scope.msg,
+      captcha : $scope.captcha
     });
 
     $scope.openGoogleMaps = function() {
@@ -19,15 +21,26 @@ angular.module('core').controller('ContactController', ['$scope', '$http',
       window.open('https://www.google.com/maps/place/University+of+Florida-+Department+of+Computer+and+Information+Science+and+Engineering/@29.6482588,-82.3456859,17z/data=!4m2!3m1!1s0x88e8a39d4f0969b5:0xd21b17bc0529867e');
     };
 
+    $scope.setResponse = function (response) {
+      data.captcha = response;
+      alert('CAPTCHA SUCCESS ' + data.captcha);
+    };
+
+    $scope.cbExpiration = function() {
+      //alert('CAPTCHA EXPIRE');
+      console.log('CAPTCHA EXPIRE');
+    };
+
     $scope.sendMail = function () {
+      alert(data.captcha);
       $http({
         method: 'POST',
         url: '/postMail', 
         data: data
       }).then(function successCallback(response) {
-
+        //alert('successCallback');
       }, function errorCallback(response) {
-
+        //alert('errorCallback');
       });
     };
   }
