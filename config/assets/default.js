633b649a5c5ec31f78c6463c36370f010683cb65
Michael Correia <michael.correia@ufl.edu>
2016-04-09 13:46:19 -0400

    Exaple Calendar displays correctly

--- config/assets/default.js
+++ config/assets/default.js
@@ -5,9 +5,12 @@ module.exports = {
     lib: {
       css: [
         'public/lib/bootstrap/dist/css/bootstrap.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.css'
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/fullcalendar/dist/fullcalendar.min.css'
       ],
       js: [
+        'public/lib/jquery/dist/jquery.min.js',
+        'public/lib/moment/min/moment.min.js',
         'public/lib/angular/angular.js',
         'public/lib/angular-resource/angular-resource.js',
         'public/lib/angular-animate/angular-animate.js',
@@ -17,7 +20,9 @@ module.exports = {
         'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
         'public/lib/angular-file-upload/angular-file-upload.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-        'public/lib/ngmap/build/scripts/ng-map.min.js'
+        'public/lib/ngmap/build/scripts/ng-map.min.js',
+        'public/lib/angular-ui-calendar/src/calendar.js',
+        'public/lib/fullcalendar/dist/fullcalendar.min.js'
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
