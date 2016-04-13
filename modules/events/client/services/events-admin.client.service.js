a272c16ede952e9db958fc0ea59215b39d2e52f1
elsenorchris <elsenorchris@aol.com>
2016-04-13 00:10:02 -0400

    fixed edit event page
    more comments

--- modules/events/client/services/events-admin.client.service.js
+++ modules/events/client/services/events-admin.client.service.js
@@ -2,6 +2,8 @@
 
 angular.module('events.admin').factory('eventsAdmin', ['$resource',
   function ($resource) {
+
+    //to update events
     return $resource('api/events/:eventId', {
       eventId: '@_id'
     },
