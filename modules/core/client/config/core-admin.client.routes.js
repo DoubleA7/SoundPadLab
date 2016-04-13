0c962de426f03bbb757ec79292be7e7c5abc7e67
Ariel <arif93@ufl.edu>
2016-04-13 13:50:11 -0400

    missing states during merging

--- modules/core/client/config/core-admin.client.routes.js
+++ modules/core/client/config/core-admin.client.routes.js
@@ -121,6 +121,42 @@ angular.module('core.admin.routes').config(['$stateProvider',
         data: {
           roles: ['admin']
         }
+      })
+		.state('admin.participants', {
+        url: '/participants',
+        templateUrl: 'modules/participants/client/views/admin/list-participants.client.view.html',
+        controller: 'ParticipantListController'
+      })
+      .state('admin.participant', {
+        url: '/participants/:participantId',
+        templateUrl: 'modules/participants/client/views/admin/view-participant.client.view.html',
+        controller: 'ParticipantController',
+        resolve: {
+          participantResolve: ['$stateParams', 'participantsAdmin', function ($stateParams, participantsAdmin) {
+            return participantsAdmin.get({
+              participantId: $stateParams.participantId
+            });
+          }]
+        }
+      })
+      .state('admin.participant-edit', {
+        url: '/participants/:participantId/edit',
+        templateUrl: 'modules/participants/client/views/admin/edit-participant.client.view.html',
+        controller: 'ParticipantController',
+        resolve: {
+          participantResolve: ['$stateParams', 'participantsAdmin', function ($stateParams, participantsAdmin) {
+            return participantsAdmin.get({
+              participantId: $stateParams.participantId
+            });
+          }]
+        }
+      })
+      .state('authentication.addParticipant', {
+        url: '/addParticipant',
+        templateUrl: 'modules/participants/client/views/admin/add-participant.html',
+        data: {
+          roles: ['admin']
+        }
       });
   }
 ]);
