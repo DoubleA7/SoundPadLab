7e976d40df9fc741ce99eb01e225ad5610d6c3fb
trident-xL <aq.blackmist@gmail.com>
2016-04-12 21:49:56 -0400

    added research tile img uploader. Warning: does not work! server is not saving the image as expected

--- modules/research/server/controllers/research.server.controller.js
+++ modules/research/server/controllers/research.server.controller.js
@@ -4,8 +4,9 @@
 var path = require('path'),
   mongoose = require('mongoose'),
   Research = require('../models/research.server.model.js'),
-  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller'));
-
+  errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
+  multer = require('multer'),
+  config = require(path.resolve('./config/config'));
 //Create
 exports.create = function (req, res) {
   var research = new Research(req.body);
@@ -22,6 +23,33 @@ exports.create = function (req, res) {
     }
   });
 };
+
+/**
+ * Update profile picture
+ */
+exports.changePicture = function (req, res) {
+  console.log("hiya");
+  console.log(req.user);
+  var user = req.user;
+  var message = null;
+  var upload = multer(config.uploads.eventUpload).single('newResearchPicture');
+  var profileUploadFileFilter = require(path.resolve('./config/lib/multer')).profileUploadFileFilter;
+  
+  // Filtering to upload only images
+  upload.fileFilter = profileUploadFileFilter;
+
+  upload(req, res, function (uploadError) {
+    if(uploadError) {
+      console.log(uploadError);
+      return res.status(400).send({
+        message: 'Error occurred while uploading profile picture'
+      });
+    } else {
+      res.json(config.uploads.eventUpload.dest + req.file.filename);
+    }
+  });
+};
+
 
 //Read
 exports.read = function (req, res) {
